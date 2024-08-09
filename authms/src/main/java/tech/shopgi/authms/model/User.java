719dbe1d4b52b207d14e6863d1ddc3589a7b466b
Luiz Almeida <luizalmeida.ads@gmail.com>
2024-08-09 19:40:34 -0300

    feat(auth-ms): add model for auth-ms

---
+++ authms/src/main/java/tech/shopgi/authms/model/User.java
@@ -0,0 +1,28 @@
+package tech.shopgi.authms.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role;
+}
