ac1c342e94e777267916eab35868c56e979e3183
Luiz Almeida <luizalmeida.ads@gmail.com>
2024-08-13 00:13:21 -0300

    test(auth-ms): add login test to auth service layer

--- authms/src/test/java/tech/shopgi/authms/service/AuthServiceTest.java
+++ authms/src/test/java/tech/shopgi/authms/service/AuthServiceTest.java
@@ -3,13 +3,17 @@ package tech.shopgi.authms.service;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.api.function.Executable;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.MethodArgumentNotValidException;
+import tech.shopgi.authms.dto.LoginRequestDto;
 import tech.shopgi.authms.dto.RegisterRequestDto;
 import tech.shopgi.authms.dto.UpdateRequestDto;
 import tech.shopgi.authms.model.User;
@@ -19,8 +23,8 @@ import tech.shopgi.authms.security.JwtTokenProvider;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class AuthServiceTest {
@@ -34,8 +38,40 @@ class AuthServiceTest {
     @Mock
     private JwtTokenProvider jwtTokenProvider;
 
+    @Mock
+    private AuthenticationManager manager;
+
     @InjectMocks
     private AuthService authService;
+
+    @Test
+    public void AuthService_Login_ReturnToken() {
+        LoginRequestDto loginRequestDto = new LoginRequestDto("username", "password");
+        User user = new User("username", "password", List.of("ROLE_USER"));
+        var authToken = new UsernamePasswordAuthenticationToken(loginRequestDto.username(), loginRequestDto.password());
+        var authentication = mock(Authentication.class);
+        String expectedToken = "token";
+
+        when(manager.authenticate(authToken)).thenReturn(authentication);
+        when(authentication.getPrincipal()).thenReturn(user);
+        when(authService.generateToken(user)).thenReturn(expectedToken);
+
+        var token = authService.login(loginRequestDto);
+
+        Assertions.assertEquals(expectedToken, token);
+        verify(manager).authenticate(authToken);
+    }
+
+    @Test
+    public void AuthService_Login_ThrowAuthenticationException() {
+        LoginRequestDto loginRequestDto = new LoginRequestDto("invalidUsername", "invalidPassword");
+        var authToken = new UsernamePasswordAuthenticationToken(loginRequestDto.username(), loginRequestDto.password());
+
+        when(manager.authenticate(authToken)).thenThrow(new BadCredentialsException("Invalid username or password."));
+
+        assertThrows(AuthenticationException.class, () -> authService.login(loginRequestDto));
+        verify(manager).authenticate(authToken);
+    }
 
     @Test
     public void AuthService_Register_ReturnRegisteredUser() throws InvalidUserInformationException {
@@ -93,7 +129,7 @@ class AuthServiceTest {
         User user = new User("username", "password", List.of("ROLE_USER", "ROLE_ADMIN"));
         User expectedUpdatedUser = new User("newUsername", "encryptedPassword", List.of("ROLE_USER", "ROLE_ADMIN"));
 
-        when(authService.loadUserByUsername("username")).thenReturn(user);
+        when(userRepository.findByUsername("username")).thenReturn(user);
         when(userRepository.save(Mockito.any(User.class))).thenReturn(expectedUpdatedUser);
         when(passwordEncoder.encode("newPassword")).thenReturn("encryptedPassword");
 
