e0d9113553d5410bf00146b74ddf4f0db5e82b5b
Luiz Almeida <luizalmeida.ads@gmail.com>
2024-08-13 23:37:44 -0300

    refactor(auth-ms): change id in model from long to UUID

--- authms/src/main/java/tech/shopgi/authms/repository/UserRepository.java
+++ authms/src/main/java/tech/shopgi/authms/repository/UserRepository.java
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import tech.shopgi.authms.model.User;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
     UserDetails findByUsername(String username);
 }
--- authms/src/main/resources/db/migration/V2__create-table-user-roles.sql
+++ authms/src/main/resources/db/migration/V2__create-table-user-roles.sql
@@ -1,5 +1,5 @@
 CREATE TABLE user_roles (
-    user_id BIGINT NOT NULL,
+    user_id UUID NOT NULL,
     role VARCHAR(255) NOT NULL,
     PRIMARY KEY (user_id, role),
     CONSTRAINT fk_user_roles_user_id FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
