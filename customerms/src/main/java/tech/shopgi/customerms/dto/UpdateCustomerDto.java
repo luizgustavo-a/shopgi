426a6f38a04ff5bb4e32b707770c605e1da36b14
Luiz Almeida <luizalmeida.ads@gmail.com>
2024-08-13 22:55:56 -0300

    refactor(customer-ms): update regex for date of birth

--- customerms/src/main/java/tech/shopgi/customerms/dto/UpdateCustomerDto.java
+++ customerms/src/main/java/tech/shopgi/customerms/dto/UpdateCustomerDto.java
@@ -7,7 +7,7 @@ public record UpdateCustomerDto(
         String name,
         @Pattern(regexp = "^\\d{3}\\.?\\d{3}\\.?\\d{3}-?\\d{2}$")
         String document,
-        @Pattern(regexp = "^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[0-2])/([0-9]{4})$")
+        @Pattern(regexp = "^(0[1-9]|[12][0-9]|3[01])([-/])(0[1-9]|1[0-2])\\2(\\d{4})$")
         String dateOfBirth,
         String gender,
         AddressDto address,
